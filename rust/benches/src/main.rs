54762e21f5b39c7a429b0cef414b2c64f55f2c59
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-07-11 05:35:15 +0300

    feat: more benches

--- rust/benches/src/main.rs
+++ rust/benches/src/main.rs
@@ -1,100 +1,102 @@
-use std::{
-    array::IntoIter,
+use std::array::IntoIter;
+
+use harmony_rust_sdk::{
+    api::{
+        auth::{auth_step::Step, next_step_request::form_fields::Field},
+        chat::{EventSource, GetGuildListRequest, InviteId, JoinGuildRequest},
+        exports::hrpc::futures_util::future::try_join_all,
+    },
+    client::{
+        api::{
+            auth::*,
+            chat::{
+                channel::get_guild_channels,
+                guild::{create_guild, get_guild_list, join_guild, CreateGuild},
+                invite::{create_invite, CreateInvite},
+                message::*,
+                GuildId,
+            },
+        },
+        error::*,
+        *,
+    },
+};
+use tokio::{
+    task::JoinError,
     time::{Duration, Instant},
 };
-
-use harmony_rust_sdk::client::{
-    api::{auth::*, chat::message::*},
-    error::*,
-    *,
-};
-use tokio::task::JoinHandle;
 
 const SERVER_ADDR: &str = "https://localhost:2289";
 const PASSWORD: &str = "123456789Ab";
 
-const BENCH_DATA: &str = include_str!("../bench_data");
-
 #[derive(Copy, Clone, Default)]
-struct BenchData<'a> {
-    id: &'a str,
-    email: &'a str,
+struct BenchData {
     guild_id: u64,
     channel_id: u64,
 }
 
 #[tokio::main]
 async fn main() -> ClientResult<()> {
-    let mut args = std::env::args().skip(1);
-    let id = args.next().unwrap();
+    match std::env::args().nth(1).unwrap().as_str() {
+        "send_messages" => {
+            let datas = (1..=4)
+                .map(|num| format!("test{}@test.org", num))
+                .collect::<Vec<_>>();
 
-    let data = BenchData {
-        id: Box::leak(id.into_boxed_str()),
-        ..Default::default()
-    };
-
-    let datas = BENCH_DATA
-        .lines()
-        .enumerate()
-        .map(|(index, line)| {
-            let mut split = line.split_whitespace();
-            (
-                index + 1,
-                (
-                    split.next().unwrap().parse().unwrap(),
-                    split.next().unwrap().parse().unwrap(),
-                ),
+            let (first, second, third, fourth) = tokio::try_join!(
+                bench_send_msgs(datas[0].as_str()),
+                bench_send_msgs(datas[1].as_str()),
+                bench_send_msgs(datas[2].as_str()),
+                bench_send_msgs(datas[3].as_str()),
             )
-        })
-        .map(|(num, (guild_id, channel_id))| BenchData {
-            email: Box::leak(format!("test{}@test.org", num).into_boxed_str()),
-            guild_id,
-            channel_id,
-            ..data
-        })
-        .collect::<Vec<_>>();
+            .unwrap();
 
-    let (first, second, third, fourth) = tokio::try_join!(
-        bench_send_msgs(datas[0]),
-        bench_send_msgs(datas[1]),
-        bench_send_msgs(datas[2]),
-        bench_send_msgs(datas[3]),
-    )
-    .unwrap();
+            let average = calc_average([first?, second?, third?, fourth?]);
 
-    let average = calc_average([first?, second?, third?, fourth?]);
+            println!(
+                "send messages results average warmup:\n10 msgs: {}\n100 msgs: {}\n1000 msgs: {}",
+                average[0].as_secs_f64(),
+                average[1].as_secs_f64(),
+                average[2].as_secs_f64(),
+            );
 
-    println!(
-        "{} send messages results average warmup:\n10 msgs: {}\n100 msgs: {}\n1000 msgs: {}\n10000 msgs: {}",
-        data.id,
-        average[0].as_secs_f64(),
-        average[1].as_secs_f64(),
-        average[2].as_secs_f64(),
-        average[3].as_secs_f64()
-    );
+            let mut average = [Duration::ZERO; 3];
 
-    let mut average = [Duration::ZERO; 4];
+            for _ in 0..10 {
+                let (first, second, third, fourth) = tokio::try_join!(
+                    bench_send_msgs(datas[0].as_str()),
+                    bench_send_msgs(datas[1].as_str()),
+                    bench_send_msgs(datas[2].as_str()),
+                    bench_send_msgs(datas[3].as_str()),
+                )
+                .unwrap();
 
-    for _ in 0..10 {
-        let (first, second, third, fourth) = tokio::try_join!(
-            bench_send_msgs(datas[0]),
-            bench_send_msgs(datas[1]),
-            bench_send_msgs(datas[2]),
-            bench_send_msgs(datas[3]),
-        )
-        .unwrap();
+                average = calc_average([average, first?, second?, third?, fourth?]);
+            }
 
-        average = calc_average([average, first?, second?, third?, fourth?]);
+            println!(
+                "send messages results average:\n10 msgs: {} secs\n100 msgs: {} secs\n1000 msgs: {} secs",
+                average[0].as_secs_f64(),
+                average[1].as_secs_f64(),
+                average[2].as_secs_f64(),
+            );
+        }
+        "smoketest" => {
+            let run = bench_many_clients().await?;
+            println!(
+                "smoketest with 1000 clients x 1000 messages -> 1000 different guild/channel: took {} secs",
+                run.as_secs_f64(),
+            );
+        }
+        "single_guild" => {
+            let run = bench_many_clients_single_guild().await?;
+            println!(
+                "1000 clients x 1000 messages -> 1 guild with event streams: took {} secs",
+                run.as_secs_f64()
+            );
+        }
+        x => println!("no such test as {}", x),
     }
-
-    println!(
-        "{} send messages results average:\n10 msgs: {}\n100 msgs: {}\n1000 msgs: {}\n10000 msgs: {}",
-        data.id,
-        average[0].as_secs_f64(),
-        average[1].as_secs_f64(),
-        average[2].as_secs_f64(),
-        average[3].as_secs_f64()
-    );
 
     Ok(())
 }
@@ -110,26 +112,101 @@ fn calc_average<const N: usize, const L: usize>(arrs: [[Duration; N]; L]) -> [Du
     temp
 }
 
-fn bench_send_msgs(data: BenchData<'static>) -> JoinHandle<ClientResult<[Duration; 4]>> {
-    tokio::spawn(async move {
-        let sent_10_msg = send_messages(10, data).await?;
-        let sent_100_msg = send_messages(100, data).await?;
-        let sent_1000_msg = send_messages(1000, data).await?;
-        let sent_10000_msg = send_messages(10000, data).await?;
-        println!(
-            "{} send messages results:\n10 msgs: {}\n100 msgs: {}\n1000 msgs: {}\n10000 msgs: {}",
-            data.id,
-            sent_10_msg.as_secs_f64(),
-            sent_100_msg.as_secs_f64(),
-            sent_1000_msg.as_secs_f64(),
-            sent_10000_msg.as_secs_f64()
-        );
-        Ok([sent_10_msg, sent_100_msg, sent_1000_msg, sent_10000_msg])
-    })
+async fn bench_many_clients_single_guild() -> ClientResult<Duration> {
+    let mut clients = Vec::with_capacity(1000);
+    let (first_client, data) = new_test_client("test1@test.org").await?;
+    let invite_id = create_invite(
+        &first_client,
+        CreateInvite::new(InviteId::new("test").unwrap(), -1, data.guild_id),
+    )
+    .await?
+    .name;
+    let request = JoinGuildRequest { invite_id };
+    let socket = first_client
+        .subscribe_events(vec![EventSource::Guild(data.guild_id)])
+        .await?;
+    clients.push((first_client, socket));
+    for i in 2..=1000 {
+        let client = new_test_client(format!("test{}@test.org", i).as_str())
+            .await?
+            .0;
+        if !get_guild_list(&client, GetGuildListRequest {})
+            .await?
+            .guilds
+            .iter()
+            .any(|g| g.guild_id == data.guild_id)
+        {
+            join_guild(&client, request.clone()).await?;
+        }
+        let socket = client
+            .subscribe_events(vec![EventSource::Guild(data.guild_id)])
+            .await?;
+        clients.push((client, socket));
+    }
+
+    let mut handles = Vec::with_capacity(1000);
+    for (client, mut socket) in clients {
+        let socket = tokio::spawn(async move {
+            loop {
+                socket.get_event().await;
+            }
+        });
+        let msgs = tokio::spawn(async move {
+            let since = Instant::now();
+            send_messages(1000, &client, data).await?;
+            ClientResult::<_>::Ok(since.elapsed())
+        });
+        handles.push(async move {
+            tokio::select! {
+                _ = socket => { panic!(); },
+                res = msgs => { res },
+            }
+        });
+    }
+    let run: Duration = try_join_all(handles)
+        .await
+        .unwrap()
+        .into_iter()
+        .map(|res| res.unwrap())
+        .sum();
+    Ok(run / 1000)
 }
 
-async fn send_messages(num: usize, data: BenchData<'static>) -> ClientResult<Duration> {
-    let client = create_new_client(data.email).await?;
+async fn bench_many_clients() -> ClientResult<Duration> {
+    let mut clients = Vec::with_capacity(1000);
+    for i in 1..=1000 {
+        clients.push(new_test_client(format!("test{}@test.org", i).as_str()).await?);
+    }
+    let mut handles = Vec::with_capacity(1000);
+    for (client, data) in clients {
+        handles.push(tokio::spawn(async move {
+            let since = Instant::now();
+            send_messages(1000, &client, data).await?;
+            ClientResult::<_>::Ok(since.elapsed())
+        }));
+    }
+    let run: Duration = try_join_all(handles)
+        .await
+        .unwrap()
+        .into_iter()
+        .map(|res| res.unwrap())
+        .sum();
+    Ok(run / 1000)
+}
+
+async fn bench_send_msgs(email: impl AsRef<str>) -> Result<ClientResult<[Duration; 3]>, JoinError> {
+    let (client, data) = new_test_client(email.as_ref()).await.unwrap();
+
+    tokio::spawn(async move {
+        let sent_10_msg = send_messages(10, &client, data).await?;
+        let sent_100_msg = send_messages(100, &client, data).await?;
+        let sent_1000_msg = send_messages(1000, &client, data).await?;
+        Ok([sent_10_msg, sent_100_msg, sent_1000_msg])
+    })
+    .await
+}
+
+async fn send_messages(num: usize, client: &Client, data: BenchData) -> ClientResult<Duration> {
     let since = Instant::now();
     for i in 0..num {
         send_message(
@@ -141,8 +218,7 @@ async fn send_messages(num: usize, data: BenchData<'static>) -> ClientResult<Dur
     Ok(since.elapsed())
 }
 
-async fn create_new_client(email: &str) -> ClientResult<Client> {
-    let client = Client::new(SERVER_ADDR.parse().unwrap(), None).await?;
+async fn login(client: &Client, email: &str) -> ClientResult<()> {
     client.begin_auth().await?;
     client.next_auth_step(AuthStepResponse::Initial).await?;
     client
@@ -151,5 +227,62 @@ async fn create_new_client(email: &str) -> ClientResult<Client> {
     client
         .next_auth_step(AuthStepResponse::login_form(email, PASSWORD))
         .await?;
-    Ok(client)
+    Ok(())
+}
+
+async fn register(client: &Client, email: &str) -> ClientResult<()> {
+    client.begin_auth().await?;
+    client.next_auth_step(AuthStepResponse::Initial).await?;
+    let register = client
+        .next_auth_step(AuthStepResponse::register_choice())
+        .await?;
+    if let Some(Step::Form(form)) = register.and_then(|s| s.step) {
+        client
+            .next_auth_step(AuthStepResponse::form(
+                form.fields
+                    .into_iter()
+                    .map(|a| match a.name.as_str() {
+                        "email" => Field::String(email.to_string()),
+                        "username" => Field::String("test".to_string()),
+                        "password" => Field::Bytes(PASSWORD.as_bytes().to_vec()),
+                        _ => panic!(),
+                    })
+                    .collect(),
+            ))
+            .await?;
+    }
+    Ok(())
+}
+
+async fn new_test_client(email: &str) -> ClientResult<(Client, BenchData)> {
+    let client = Client::new(SERVER_ADDR.parse().unwrap(), None).await?;
+    if login(&client, email).await.is_err() {
+        register(&client, email).await?;
+    }
+
+    let guild_id = if let Some(entry) = get_guild_list(&client, GetGuildListRequest {})
+        .await?
+        .guilds
+        .pop()
+    {
+        entry.guild_id
+    } else {
+        create_guild(&client, CreateGuild::new("test".to_string()))
+            .await?
+            .guild_id
+    };
+    let channel_id = get_guild_channels(&client, GuildId::new(guild_id))
+        .await?
+        .channels
+        .pop()
+        .unwrap()
+        .channel_id;
+
+    Ok((
+        client,
+        BenchData {
+            guild_id,
+            channel_id,
+        },
+    ))
 }
